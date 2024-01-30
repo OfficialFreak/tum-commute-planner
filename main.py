4b32a8f62405a0a5641cdfed667344f8ac041c55
OfficialFreak <npfreak@t-online.de>
2024-01-30 10:10:48 +0100

    Progress for MVG API Request + parsing

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 from client import CalendarClient
 import settings
 
+USER_AGENT = "tum-commute-planer/1.0"
+
 def remove_streams(events):
     return list(filter(lambda event : "Videoübertragung aus" not in event.get("description", ""), events))
 
@@ -38,7 +40,10 @@ def get_location_data(location: str):
     API_URL = "https://nav.tum.de"
     response = requests.get(
         f"{API_URL}/api/get/{location}",
-        headers={"accept": "application/json"}
+        headers={
+            "User-Agent": USER_AGENT,
+            "Accept": "application/json"
+        }
     )
     response_json = response.json()
     coords = response_json.get("coords", None)
@@ -47,16 +52,30 @@ def get_location_data(location: str):
         "link": f'{API_URL}{response_json.get("redirect_url", "")}'
     }
 
-def get_location_coords(location: str):
-    return get_location_data(location).get("coords", None)
-
 def main():
-    todays_date = date.today() + timedelta(days=3)
-    todays_events = get_events_on_day(settings.TUM_CALENDAR_ID, todays_date)
+    #todays_date = date.today() + timedelta(days=3)
+    #todays_events = get_events_on_day(settings.TUM_CALENDAR_ID, todays_date)
 
-    locations = [get_location(event) for event in todays_events]
-    print(locations)
-    print(get_location_data(locations[0]))
+    #locations = [get_location(event) for event in todays_events]
+    #print(locations)
+    #location_data = get_location_data(locations[0])
+    #print(location_data)
+    
+    arrival_time = datetime.now() + timedelta(hours=2) - timedelta(hours=1) # idk why but mvg does it as well
+    print(arrival_time.isoformat() + "Z")
+    # get route for given arrival time
+    response_json = requests.get("https://www.mvg.de/api/fib/v2/connection", params={
+        "originLatitude": 0,
+        "originLongitude": 0,
+        "destinationLatitude": 0,
+        "destinationLongitude": 0,
+        "routingDateTime": arrival_time.isoformat() + "Z",
+        "routingDateTimeIsArrival": True,
+        "transportTypes": "SCHIFF,RUFTAXI,BAHN,UBAHN,TRAM,SBAHN,BUS,REGIONAL_BUS"
+    }, headers={"User-Agent": USER_AGENT}).json()
+    print(response_json)
+    # TODO: Parsing a single Route to something like
+    # [{"departure": from.plannedDeparture, "arrival": to.plannedDeparture, "from": f"{from.name}, {from.place}", "to": f"{to.name}, {to.place}", "transportationName": f"{line.transportType} {line.label} {line.transportType}"}, ...]
 
 if __name__ == "__main__":
     main()
