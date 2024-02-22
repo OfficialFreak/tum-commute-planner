0ef8778d095073b1742096c1d1dd44c8272007e4
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2024-02-22 19:19:39 +0100

    Made Path to python exe a .env option

--- auto_update.py
+++ auto_update.py
@@ -1,6 +1,10 @@
+import os
+
 import requests
 import time
 import subprocess
+
+from dotenv import load_dotenv
 
 
 def get_latest_commit():
@@ -28,10 +32,11 @@ def main():
             print("Pulling changes")
             subprocess.run(["git", "pull"])
             print("Restarting Commute Planner")
-            script_process = subprocess.Popen([".venv/Scripts/python.exe", "-m", "commute_planner.main"])
+            script_process = subprocess.Popen([os.environ.get("VENV_PYTHON_EXECUTABLE"), "-m", "commute_planner.main"])
             latest_known_commit = latest_commit
         time.sleep(60 * 1)
 
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
