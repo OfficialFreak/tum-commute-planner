465e68a4f1f96e01f5e1520e4cb49504ad84500e
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2024-02-23 15:24:59 +0100

    added statement, to quit subprocess when shutdown

--- auto_update.py
+++ auto_update.py
@@ -20,22 +20,27 @@ def main():
     print("Starting Auto-Update Script.")
     latest_known_commit = None
     script_process = None
-    while 1:
-        latest_commit = get_latest_commit()
-        if latest_commit != latest_known_commit:
-            print("New commit found.")
-            if script_process is not None:
-                subprocess.Popen.terminate(script_process)
-            # Refresh Repo
-            print("Pulling changes")
-            subprocess.run(["git", "pull"])
-            print("Restarting Commute Planner")
-            # script_process = subprocess.Popen([
-            #   os.environ.get("VENV_PYTHON_EXECUTABLE"), "-m", "commute_planner.main"])
-            script_process = subprocess.Popen([
-                os.environ.get("VENV_PYTHON_EXECUTABLE"), "phue_connected_commute_planner.py"])
-            latest_known_commit = latest_commit
-        time.sleep(60 * 1)
+    try:
+        while 1:
+            latest_commit = get_latest_commit()
+            if latest_commit != latest_known_commit:
+                print("New commit found.")
+                if script_process is not None:
+                    subprocess.Popen.terminate(script_process)
+                # Refresh Repo
+                print("Pulling changes")
+                subprocess.run(["git", "pull"])
+                print("Restarting Commute Planner")
+                # script_process = subprocess.Popen([
+                #   os.environ.get("VENV_PYTHON_EXECUTABLE"), "-m", "commute_planner.main"])
+                script_process = subprocess.Popen([
+                    os.environ.get("VENV_PYTHON_EXECUTABLE"), "phue_connected_commute_planner.py"])
+                latest_known_commit = latest_commit
+            time.sleep(60 * 1)
+    except KeyboardInterrupt:
+        print("Shutting down...")
+        if script_process is not None:
+            subprocess.Popen.terminate(script_process)
 
 
 if __name__ == "__main__":
